f9a6502d411527e7bb6bf7870e992f740a4caa32
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-09 17:53:16 +0530

    feat: :sparkles: add get inquirer instance

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,21 @@
-// import gradient from "gradient-string";
-// const inquirer = import("inquirer");
 import { printHeading } from "utils/print-heading";
+import { getInquirer } from "utils/get-inquirer";
 
 export const createProject = async () => {
-	// const coolGradient = gradient(["#FFA559", "#FF6000"]);
-	// const myInquirer = await inquirer;
-	// const yourName = await myInquirer.default.prompt([
-	// 	{
-	// 		name: "name",
-	// 		type: "input",
-	// 		message: coolGradient("What's your name?"),
-	// 	},
-	// ]);
-	// const yourTitle = await myInquirer.default.prompt([
-	// 	{
-	// 		name: "title",
-	// 		type: "input",
-	// 		message: coolGradient("What's your last name?"),
-	// 		default: "Bhattacharya",
-	// 	},
-	// ]);
-	// console.log(coolGradient(`My name is ${yourName.name} ${yourTitle.title}`));
-
 	printHeading();
+	console.log();
+	console.log();
+
+	const inquirer = await getInquirer.inquirerInstance();
+
+	const data = await inquirer.prompt([
+		{
+			type: "input",
+			message: "What is your name?",
+			default: "Ankan",
+			name: "name",
+		},
+	]);
+
+	console.log(data.name);
 };
