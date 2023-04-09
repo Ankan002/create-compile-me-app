09a00ffef32019ea2e4982e5898e7f53d6c180b5
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-09 16:57:14 +0530

    feat: :sparkles: add print heading

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
-import gradient from "gradient-string";
-const inquirer = import("inquirer");
+// import gradient from "gradient-string";
+// const inquirer = import("inquirer");
+import { printHeading } from "utils/print-heading";
 
 export const createProject = async () => {
-	const coolGradient = gradient(["#FFA559", "#FF6000"]);
+	// const coolGradient = gradient(["#FFA559", "#FF6000"]);
+	// const myInquirer = await inquirer;
+	// const yourName = await myInquirer.default.prompt([
+	// 	{
+	// 		name: "name",
+	// 		type: "input",
+	// 		message: coolGradient("What's your name?"),
+	// 	},
+	// ]);
+	// const yourTitle = await myInquirer.default.prompt([
+	// 	{
+	// 		name: "title",
+	// 		type: "input",
+	// 		message: coolGradient("What's your last name?"),
+	// 		default: "Bhattacharya",
+	// 	},
+	// ]);
+	// console.log(coolGradient(`My name is ${yourName.name} ${yourTitle.title}`));
 
-	const myInquirer = await inquirer;
-
-	const yourName = await myInquirer.default.prompt([
-		{
-			name: "name",
-			type: "input",
-			message: coolGradient("What's your name?"),
-		},
-	]);
-
-	const yourTitle = await myInquirer.default.prompt([
-		{
-			name: "title",
-			type: "input",
-			message: coolGradient("What's your last name?"),
-			default: "Bhattacharya",
-		},
-	]);
-
-	console.log(coolGradient(`My name is ${yourName.name} ${yourTitle.title}`));
+	printHeading();
 };
