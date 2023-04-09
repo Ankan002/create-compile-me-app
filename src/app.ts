f92e14f33b22c8107637ef8dab4b663366054d05
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-09 14:47:54 +0530

    feat: :sparkles: add inquirer system

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
 import gradient from "gradient-string";
+const inquirer = import("inquirer");
 
-export const createProject = () => {
+export const createProject = async () => {
 	const coolGradient = gradient(["#FFA559", "#FF6000"]);
 
-	console.log(coolGradient("Hello GUYS!!"));
+	const myInquirer = await inquirer;
+
+	const yourName = await myInquirer.default.prompt([
+		{
+			name: "name",
+			type: "input",
+			message: coolGradient("What's your name?"),
+		},
+	]);
+
+	const yourTitle = await myInquirer.default.prompt([
+		{
+			name: "title",
+			type: "input",
+			message: coolGradient("What's your last name?"),
+			default: "Bhattacharya",
+		},
+	]);
+
+	console.log(coolGradient(`My name is ${yourName.name} ${yourTitle.title}`));
 };
