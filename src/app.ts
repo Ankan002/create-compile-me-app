72abd83f3e6463bb2ebca7ac579f955cb1983d13
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-29 10:18:41 +0530

    feat: :sparkles: windows command

--- src/app.ts
+++ src/app.ts
@@ -100,7 +100,7 @@ export const createProject = async () => {
 
 		await executeShellCommand({
 			command: `cd ${projectName} && npm pkg set name=${projectName} && ${
-				getOSPlatform().includes("win32") && !process.env["SHELL"]?.includes("bash")
+				getOSPlatform() === "win32" && !process.env["SHELL"]?.includes("bash")
 					? "rd /s /q .git && del yarn.lock package-lock.json"
 					: "rm -rf .git yarn.lock package-lock.json"
 			}`,
